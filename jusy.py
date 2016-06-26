fbb8e3eee23ba2ccdb7154863668e8c4f10fa45e
Andrew <realgrandrew@gmail.com>
2016-06-26 14:13:46 +0000

    jusy wip

--- jusy.py
+++ jusy.py
@@ -1,83 +1,47 @@
-"""
-+ send the announce call
-+ check that user can get it
-+ add startup for user
-- start # of threads as # or cores
-- implement accounting: 
-    - periodic check that we have less processes than cores
-    - fork
-    - create user
-    - launch:
-        - connection
-        - accounting loop
-- implement finish
-    - delete user
-    - umount
-    - remove
-- periodic performance-ping check using paramiko?? -> reconnect to same port!?! -> test by JSON!
-- if no I/O was possible (20 sec?) -> remove contract
-- check finish
-- account cpu
-- restartless server (+ attach to system?) http://stackoverflow.com/questions/4163964/python-is-it-possible-to-attach-a-console-into-a-running-process
-- coinbase integration
-- bitcoin transfer-out request
-"""
-import sys
+import sys,multiprocessing
 OWNER_HASH = sys.argv[1] # first parameter is owner hash
-
-import socket,select,json,threading,time,subprocess,random,os,shutil
-from pwd import getpwnam
-import multiprocessing
-
 NCORES = multiprocessing.cpu_count()
+NSESSIONS = int(NCORES * 1.3)
+
+# -----------------------------------------------
+
+import socket,select,json,threading,time,subprocess,random,os,shutil,datetime
+from os import stat
+from pwd import getpwuid
+from pwd import getpwnam
+
+import logging
+import logging.handlers
+logger = logging.getLogger('jusy')
+logger.setLevel(logging.DEBUG)
+handler = logging.handlers.SysLogHandler(address = '/dev/log')
+logger.addHandler(handler)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
 
 ESQ_SEQ_BEG = "~\'\"\"\"{~~."
 ESQ_SEQ_END = ".~~\'\"\"\"{~"
 ESQ_LEN = len(ESQ_SEQ_BEG)
 MSG_LEN = 8192
 COUNTER = 0
+USER_BEG = "jsuser"
+MAX_PROC_PER_USER = 50
 
-PRIVKEY = """
------BEGIN RSA PRIVATE KEY-----
-MIIEpQIBAAKCAQEA8mkapurvGCGwl1qZmyvQiSLQF/Sc5MxtN6V0ItPmbdvl+uqg
-2CWE2rozuddtlSo8WYxOtfVTWdP3bBt55bMRlT3WUPeQ+X2rb68r9aQqvCNUOYB/
-M6l5/TGdZUVF7jQvvCT5EW3XozS3Ue5tCqowLnFYnjacluHwdn7rYa09LdC4Z1xK
-oQAXyTsMM9QgV6QM6VM/kGwpnxk9HyhGyAIXc6vj1rKcFUm5RJiY2dMHgTDX5RdY
-h/hKING5PYPOYG+3db5Ifzueb/VtoOGNxwGqkiOOk1zKdA4FaN2T+iAiUnTIWO2C
-29csUBk1pdOpZ2up8ulBVvSguFCtn81PXWsi6wIDAQABAoIBAQDAEcU7UlSlgzQf
-iHrC9yFhN0M4Z+nUY6F6AM+XNNyUWTLM8BEHkhrFD9oiOu4pzXBJ56EKDVrAhvG5
-J8Aa3xpkbL2eI7or5IXslRg4pp3xufBnK0geT/9HPYOflRbGmXTlF0p9o9HDD+bc
-tRSS6awJcGet3EmQR283BHNOTmx2GirZo4srktx2PqOZbIwEEtlXvadfn6PuAQwj
-r8RLCTD0gewh/qESbzp/omQQGnYqm7xHt+gsIqW8JS1NygoP7vcUQ13ouxlZeM28
-Ru9vpMLpkBFbK1Us/mRz+CUqBtNxa95bgEXcnl3sCnG686OgMOcE4hN/Ak/+qzDz
-WSIe0KP5AoGBAP40TdJdDmYfoeJp6B1Go0s5zMFt1WAjGwdsSnJH8fhvZYcmEwKH
-ffA1vZVJHrkU39JGNBCyjIcpop56s7DswpBvENdPESDixlEdLmOBJwnKe7v6MCbX
-+gqeT58BarSkMVIh7jGFovdVFwQWDUKtbcVPlBNKPXUvxmD7dTCU+e+3AoGBAPQf
-eP5B2oheSYKRXoORlTiu2tqxx94Mkz4t+pjCbuK4KUWc7yelXquJsmMmigZ9OhQL
-KNVjdH+IuRA5yrUjXxsHwAM5XEIkZ/GBN34cC/Hrz/A9wkrbti8qbh+OVY1tvjwP
-CqFZ4/QrMo7ZmtZDfeJ60AdUPGX2us11zLjlHH5tAoGAdRnHF7cCYQ0FV/WNlZ7q
-rkasChb8ilLhqqO6D7cQwm8eiAmEDFA6NaOr9889NellycnJRj/Z+JG3deX0TPZk
-EoD3cxfbPhj5Xnhg3DmssBf0s/1mnR408xtGdmsCqsBQcBNtVKbJEcLC3YEDDsox
-nQkxn0k90nP1oMt1irG2CTUCgYEA2ezejdsrwmCpLq2mubzUgO1W4bOiwE5pgzB4
-bLXlVu8fqM0XkUTG5krdvDVDMdfIaOwtX5CRBuh+jEWK93jEBdU9S6OBAGqPf7/i
-UemnoCbqUYRQVZMdZ61w8SqWpNI66FJlIvSj0exeDbejyXImhm8sFd7UrBE9YPyv
-7L5aWWECgYEAyBxQyPXfIHAJ7NjvCVkXgh4IUz/kt3ZEkZJIuOpbdLqfn0e/1MNm
-6IhD2IDYoVhR8uq8JSFuwbRWjX+HNDHC1E4rZpQPEPjjTQyvX7ZHJ48lR3d4Qqt/
-bbVEZMQThUGEMq3zRTMW++srfrDmBkaN/uEfj52wTUcvJ7Pk4GsieVg=
------END RSA PRIVATE KEY-----
-"""
-
-class JuSyProxy(object):
+class JuSyProxy(threading.Thread):
     def __init__(self, session):
-        self.session = session
         self.started = False
         pass
     def send_dict(self, d):
         s = json.dumps(d)
         self.send_sock.send(ESQ_SEQ_BEG+s+ESQ_SEQ_END)
     def handle_connect(self):
-        self.send_dict({"type": "announce", "username": self.session.username, "privkey": self.session.privkey, "owner_hash": OWNER_HASH })
-    def loop(self):
+        pass
+    def finish(self, fin_code):
+        pass
+    def stop(self):
+        self._loop = False
+    def run(self):
         s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s1.connect(("localhost", 8022))
         s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -88,9 +52,10 @@ class JuSyProxy(object):
         data = ""
         msg_wait = False
         msg = ""
+        self._loop = True
         
-        while True:
-            readable, writable, exceptional = select.select(inputs, outputs, inputs)
+        while self._loop:
+            readable, writable, exceptional = select.select(inputs, outputs, inputs, timeout=2)
             for s in readable:
                 if not self.started:
                     self.started = True
@@ -127,16 +92,20 @@ class JuSyProxy(object):
                                 msg_wait = False
                     if read: s1.send(read)
             # print "Received:", len(data)
-            if len(data) == 0:
+            if readable and len(data) == 0:
                 break
             if exceptional: 
                 break
         print "Connection closed."
-        s.close()
+        s1.close()
+        s2.close()
+        if self._loop:
+            self.finish("FIN_CONN_CLOSED") # it is obviously impossible to send fin status if connection is closed...
  
 
-class JSSession(object):
+class JSSession(JuSyProxy):
     def __init__(self):
+        super(JSSession, self).__init__()
         self.username = ""
         self.privkey = "" # will be filled by get_privkey_access
         self.gen_user()
@@ -144,10 +113,15 @@ class JSSession(object):
         self.create_disk()
         self.mount_disk()
         self.gen_privkey_access()
+        self.accounting_start_ts = 0
+        self.session_start_ts = time.time()
+        
+    def handle_connect(self):
+        self.send_dict({"type": "announce", "username": self.username, "privkey": self.privkey, "owner_hash": OWNER_HASH })
     
     def gen_user(self):
         salt = random.randint(100000, 99999)
-        self.username = "user"+str(salt)+str(COUNTER)
+        self.username = USER_BEG+str(salt)+str(COUNTER)
         COUNTER += 1
         subprocess.call(["addgroup", "nobody"])
         subprocess.call(["adduser", "--disabled-password", "--gecos", "", "--ingroup", "nobody", self.username])
@@ -173,25 +147,126 @@ class JSSession(object):
         os.chown(os.path.join(self.home, ".ssh", "authorized_keys"), self.uid, self.gid)
         os.chmod(os.path.join(self.home, ".ssh", "authorized_keys"), 0o644)
     
-    def run(self):
-        self.jproxy = JuSyProxy(self)
-        self.t = threading.Thread(target=self.jproxy.loop)
-        self.t.start()
+    def check_accounting(self):
+        "implements all the accounting requirements"
+        proccount = count_processes(self.username)
+        if self.accounting_start_ts == 0 and proccount > 0:
+            self.accounting_start_ts = time.time()
+        cputime = 0
+        cputime += count_cpu_time_live(self.username)
+        cputime += count_cpu_time_past(self.username)
+        if cputime > 3600:
+            logger.info("max work reached for %s - stopping" % self.username)
+            self.finish("FIN_DONE")
+            return
+        if proccount > MAX_PROC_PER_USER:
+            self.finish("FIN_MAXPROC_EXCEEDED")
+            return
+        ram_kb = count_rss_kb_unsafe(self.username)
+        if ram_kb > 2e6:
+            self.finish("FIN_RAM_EXCEEDED")
+            return
+        if time.time() - self.accounting_start_ts > 3600 * 3 and cputime < 500:
+            self.finish("FIN_IDLE")
+            return
         
-        self.start_accounting()
+        
+        
+        
+class Accounting(object):
+    def __init__(self, session):
+        self.session = session
+        # now start acct http://www.cyberciti.biz/tips/howto-log-user-activity-using-process-accounting.html
+        
+# ----------------------------------------------------
+# accounting utility functions
+# ----------------------------------------------------
+
+def check_acct():
+    try:
+        subprocess.check_output(["accton"])
+    except OSError:
+        # error: acct, sa not
+        return False
+    finally:
+        pass
+    try:
+        subprocess.check_output(["sa", "-m"])
+    except subprocess.CalledProcessError:
+        return False
+    return True
+
+def find_owner(filename):
+    return getpwuid(stat(filename).st_uid).pw_name
+
+def count_cpu_time_live(username):
+    total = 0
+    try:
+        for l in subprocess.check_output(["top", "-b", "-n", "1", "-u", username]).split("\n")[7:]:
+            tt = l.split()
+            if len(tt) < 11: continue
+            x = time.strptime(tt[10].split(".")[0], '%M:%S')
+            total += datetime.timedelta(hours=x.tm_hour,minutes=x.tm_min,seconds=x.tm_sec).total_seconds()
+    except subprocess.CalledProcessError:
+        pass
+    return total
+
+def count_rss_kb_unsafe(username):
+    o = subprocess.check_output("top -b -n 1 -u %s | awk -v var=\"%s\" 'NR>7 { sumC += $9; }; { sumM += $6; } END { print sumM; }'" % (username,username), shell=True)
+    if not o: return 0
+    return int(o)
+
+def start_acct():
+    # will only need this for RPM distros
+    subprocess.call("chkconfig psacct on", shell=True)
+    subprocess.call("chkconfig acct on", shell=True)
+    subprocess.call("/etc/init.d/psacct start", shell=True)
+    subprocess.call("/etc/init.d/acct start", shell=True)
     
-    def start_accounting(self):
-        raise NotImplementedError
-        
-        
-        
+def count_cpu_time_past(username):
+    for l in subprocess.check_output(["sa", "-m"]).split("\n"):
+        if username in l:
+            return float(l.split()[3][:-2])*60
+    return 0
+    
+def count_processes(username):
+    import os
+    pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
+    c = 0
+    for pid in pids:
+        try:
+            if find_owner(os.path.join('/proc', pid)) == username:
+                c += 1
+        except IOError: # proc has already terminated
+            continue
+    return c
+
 class Worker(object):
     def __init__(self):
-        pass
+        self.sessions = []
+    
+    def count_sessions(self):
+        self.sessions = [t for t in self.sessions if not t.isAlive()]
+        return len(self.sessions)
+    
+    def start_session(self):
+        s = JSSession()
+        s.start()
+        self.sessions.append(s)
+    
+    def system_load(self):
+        return os.getloadavg()[0]
+    
+    def check_cpu_times(self):
+        for s in self.sessions:
+            s.check_accounting()
     
     def loop(self):
         while True:
-            time.sleep()
+            if self.count_sessions() < NSESSIONS and self.system_load() < NCORES*1.3:
+                self.start_session()
+            self.check_cpu_times()
+            time.sleep(5)
 
     
     
