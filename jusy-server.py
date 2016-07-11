2358d3be2e37037bdf103dfce041b5ec71f2cc5b
Andrew <realgrandrew@gmail.com>
2016-07-11 13:23:10 +0000

    version bump

--- jusy-server.py
+++ jusy-server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-__version__ = "0.15"
+__version__ = "0.16"
 __scripturl__ = "https://raw.githubusercontent.com/junk-systems/jusy/master/jusy-server.py"
 __author__ = "Andrew Gryaznov"
 __copyright__ = "Copyright 2016, Junk.Systems"
